945ad9345b7b989b175380f1994b900093b3490c
quanzhi <qf37@duke.edu>
2024-03-29 23:36:53 -0400

    add game logic and test

--- gameserver/test/gomoku/gomoku.test.ts
+++ gameserver/test/gomoku/gomoku.test.ts
@@ -4,7 +4,7 @@ import { expect } from 'chai';
 
 
 describe('Gomoku Game Logic', () => {
-  let game: Gomoku;
+  let game : Gomoku;
 
   beforeEach(() => {
     game = new Gomoku();
@@ -47,12 +47,14 @@ describe('Gomoku Game Logic', () => {
 
   it('should detect a draw when the board is full', () => {
     for (let x = 0; x < Gomoku.getBoardSize(); x++) {
+      let color = (x % 2 === 0) ? 1 : 2;
       for (let y = 0; y < Gomoku.getBoardSize(); y++) {
-        game.allocPiece({ x, y }, y % 2 === 0 ? 1 : 2);
+        if (y % 3 === 0) {
+          color = (color === 1) ? 2 : 1;
+        }
+        game.allocPiece({ x, y }, color);
       }
     }
-    // Manually overwrite the last piece to not trigger a win condition
-    game.allocPiece({ x: Gomoku.getBoardSize() - 1, y: Gomoku.getBoardSize() - 1 }, 1);
     expect(game.getWinner()).to.equal(0); // Draw
   });
 
